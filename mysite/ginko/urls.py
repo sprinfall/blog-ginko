0788504bcd372c5edcfd73410e78b4f903fef3a5
Chunting Gu <sprinfall@gmail.com>
2018-03-22 17:28:10 +0800

    Follow tutorial part 4

--- mysite/ginko/urls.py
+++ mysite/ginko/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+# Namespace for URLconf.
+# Django needs this to know which app view to create for a url when using
+# the {% url %} template tag.
+app_name = "ginko"
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- mysite/ginko/views.py
+++ mysite/ginko/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+
+from .models import Post
 
 
-def index(request):
-    return HttpResponse("Hello, world!")
+class IndexView(generic.ListView):
+    # Default template name: post_list.html
+    template_name = 'ginko/index.html'
+    # Default context variable is `post_list`
+    context_object_name = 'latest_post_list'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-create_time')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Post
+    # Default template name: post_detail.html
+    template_name = 'ginko/detail.html'
+
